c1e0b9511a7a600fbf6dc0dea939f0bcb8b45dfb
Cristian <cristianlarrauri1@gmail.com>
2023-05-15 15:18:55 -0300

    Add view Home

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import GitHub from "../components/GitHub.jsx";
+import imgPortfolio from "../images/imgPrueba1.png";
+
+export default function Home() {
+  return (
+    <>
+      <section className="flex justify-center gap-5 p-10">
+        <div className="animate__animated animate__fadeInDown animate__delay-5s">
+          <img
+            src={imgPortfolio}
+            alt="imagen portada"
+            className="rounded-full w-auto h-80 border border-lilaClaro-0"
+          />
+        </div>
+        <div className="flex justify-center text-center items-center p-10 text-grisClaro-0 flex-col">
+          <h1 className="text-4xl font-bold animate__animated animate__fadeInDown">
+            Hola! Mi nombre es Cristian
+          </h1>
+          <h2 className="text-xl text-lilaClaro-0 animate__animated animate__fadeInDown animate__delay-1s">
+            vivo en Córdoba-Argentina y soy desarrollador Full-Stack.
+          </h2>
+          <p className="text-grisClaro-0 text-sm animate__animated animate__fadeInDown animate__delay-2s">
+            Tengo experiencia en liderazgo de grupos en distintos ambitos, por
+            lo cual la organizacion y el trabajo en equipo son mi fuerte.
+          </p>
+          <p className="text-grisClaro-0 text-sm animate__animated animate__fadeInDown animate__delay-3s">
+            En mi encontraras una persona proactiva, responsable y con la
+            curiosidad como motor del aprendizaje.
+          </p>
+        </div>
+      </section>
+      <section className="animate__animated animate__fadeInDown animate__delay-4s">
+        <GitHub />
+      </section>
+    </>
+  );
+}
