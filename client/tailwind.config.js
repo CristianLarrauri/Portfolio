5838b3981c7ca460ca09bf95fe50a3cf832273e3
Cristian <cristianlarrauri1@gmail.com>
2023-05-15 15:17:41 -0300

    Add initial config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'grisClaro': ['#dce0e6'],
+      'grisOscuro': ['#606d80'],
+      'lilaClaro': ['#567ebb'],
+      'lilaOscuro': ['#2b4c7e'],
+      'negro': ['#1f1f20'],
+    }},
   },
   plugins: [],
-};
+}
