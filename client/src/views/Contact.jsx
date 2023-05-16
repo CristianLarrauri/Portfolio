c20c7e16ffbecc3dbcc11f047637009be104c3f5
Cristian <cristianlarrauri1@gmail.com>
2023-05-16 00:14:09 -0300

    Fix view Contact - styles

--- client/src/views/Contact.jsx
+++ client/src/views/Contact.jsx
@@ -4,10 +4,10 @@ import FormContact from "../components/FormContact";
 export default function Contact() {
   return (
     <>
-      <header>
+      <header className="flex flex-col justify-center items-center text-4xl font-bold text-grisClaro-0 p-5">
         <h1>Contacta conmigo</h1>
       </header>
-      <section>
+      <section className="flex justify-center gap-7 p-10">
         <FormContact />
       </section>
     </>
