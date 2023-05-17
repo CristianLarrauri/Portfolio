a16f97a20860342e4307d9665d1dd4fa6d644811
Cristian <cristianlarrauri1@gmail.com>
2023-05-16 23:29:41 -0300

    Fix component CardAboutTech - style flip

--- client/src/components/CardAboutTech.jsx
+++ client/src/components/CardAboutTech.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "animate.css";
 
 export default function CardAboutTech({ icon, title, content }) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleCardClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className="border border-lilaClaro-0 w-80 h-80 flex flex-col justify-between text-center items-center p-6 rounded-2xl">
-      <header className="flex flex-col justify-between items-center h-3/5 w-3/5">
+    <div
+      className={`border border-lilaClaro-0 w-80 h-80 flex flex-col justify-between text-center items-center p-6 rounded-2xl hover:scale-105 ${
+        isFlipped ? "animate__animated animate__flipInY" : ""
+      }`}
+      onClick={handleCardClick}
+    >
+      <header
+        className={`flex flex-col justify-between items-center h-3/5 w-3/5 ${
+          isFlipped ? "hidden" : ""
+        }`}
+      >
         <div className="text-grisClaro-0 w-2/3 h-2/3 flex justify-center items-center">
           {icon}
         </div>
         <h2 className="font-bold text-xl text-lilaClaro-0">{title}</h2>
       </header>
-      <section>
+      <section
+        className={`${
+          isFlipped ? "" : "hidden"
+        } animate__animated animate__fadeIn`}
+      >
         {Array.isArray(content) ? (
           content.map((element, index) => <p key={index}>{element}</p>)
         ) : (
