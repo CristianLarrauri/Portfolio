20535769a51bf37e0a1416f8959d97a5a9a986b0
Cristian <cristianlarrauri1@gmail.com>
2023-05-16 22:00:20 -0300

    Fix component CardAboutText / styles

--- client/src/components/CardAboutTech.jsx
+++ client/src/components/CardAboutTech.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-export default function CardAboutTech({ img, title, content }) {
+export default function CardAboutTech({ icon, title, content }) {
   return (
-    <div className="border border-lilaClaro-0 w-80 h-80 flex flex-col justify-center items-center p-4 rounded-2xl">
-      <img src={img} alt="imagen card" />
-      <h2>{title}</h2>
-      <p>{content}</p>
+    <div className="border border-lilaClaro-0 w-80 h-80 flex flex-col justify-between text-center items-center p-6 rounded-2xl">
+      <header className="flex flex-col justify-between items-center h-3/5 w-3/5">
+        <div className="text-grisClaro-0 w-2/3 h-2/3 flex justify-center items-center">
+          {icon}
+        </div>
+        <h2 className="font-bold text-xl text-lilaClaro-0">{title}</h2>
+      </header>
+      <section>
+        {Array.isArray(content) ? (
+          content.map((element, index) => <p key={index}>{element}</p>)
+        ) : (
+          <p>{content}</p>
+        )}
+      </section>
     </div>
   );
 }
