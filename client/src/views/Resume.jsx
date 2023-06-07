3ca8e8000c42fa369fa333dbc478371244d690b0
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-06-07 01:13:52 -0300

    Fix Resume

--- client/src/views/Resume.jsx
+++ client/src/views/Resume.jsx
@@ -7,7 +7,7 @@ export default function Resume() {
       <header className="h-full w-full flex flex-col justify-center items-center text-4xl font-bold text-grisClaro-0 p-5">
         <h1>Hoja de vida</h1>
       </header>
-      <section className="border border-red-600 h-full w-full flex justify-center gap-7 p-10">
+      <section className="w-full flex justify-center gap-7 p-10">
         <PdfResume />
       </section>
     </div>
