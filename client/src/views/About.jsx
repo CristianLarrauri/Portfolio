c6bfb8f5ca6c64d0bb03bd9605b8085bafec7a51
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-06-21 13:38:05 -0300

    Fix styles

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -11,7 +11,7 @@ export default function About() {
       <header className="h-full w-full flex flex-col justify-center items-center text-4xl font-bold text-grisClaro-0 p-5">
         <h1>Tecnologías y habilidades</h1>
       </header>
-      <section className="h-full w-full p-10 flex flex-col lg:flex-row justify-center items-center text-center gap-5 text-white">
+      <section className="h-full w-full p-10 flex flex-col lg:flex-row justify-center items-center text-center gap-5 text-grisClaro-0">
         <CardAboutTech
           icon={<AiFillTool className="w-full h-full" />}
           title="Herramientas"
