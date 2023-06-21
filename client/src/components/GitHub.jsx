520bff103ca7c467f93dbe47d7392410d0a8fa05
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-06-21 11:38:09 -0300

    Fix view GitHub

--- client/src/components/GitHub.jsx
+++ client/src/components/GitHub.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import GitHubCalendar from "react-github-calendar";
 
 export default function GitHub() {
-  const theme = {
-    dark: ["#1f1f20", "#84a5d4", "#5281be", "#2c5c9f", "#1d3f7c"],
-  };
+  // const theme = {
+  //   dark: ["#1f1f20", "#84a5d4", "#5281be", "#2c5c9f", "#1d3f7c"],
+  // };
   return (
     <div className="flex justify-center p-10 text-grisClaro-0">
       <GitHubCalendar
