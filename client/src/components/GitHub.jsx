faf91c1be202f88aa42a74fcc2fe755f3ccad71c
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-06-21 12:10:08 -0300

    Add title GitHub Calendar

--- client/src/components/GitHub.jsx
+++ client/src/components/GitHub.jsx
@@ -6,7 +6,10 @@ export default function GitHub() {
     dark: ["#1f1f20", "#0a3069", "#0969da", "#54aeff", "#b6e3ff"],
   };
   return (
-    <div className="flex justify-center p-10 text-grisClaro-0">
+    <div className="h-full w-full flex flex-col justify-center items-center text-center text-grisClaro-0">
+      <h1 className="h-full w-full p-10 text-2xl text-lilaClaro-0">
+        GitHub Calendar
+      </h1>
       <GitHubCalendar
         username="CristianLarrauri"
         blockSize={15}
