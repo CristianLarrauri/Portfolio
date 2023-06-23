485e5278a42261594c885e1b3bc2fa750849a2ff
CristianLarrauri <cristianlarrauri1@gmail.com>
2023-06-22 21:35:27 -0300

    Fix PdfResume - Fix button prev and next

--- client/src/components/PdfResume.jsx
+++ client/src/components/PdfResume.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import pdfUrl from "../assets/Resume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 
 const PDFPreview = () => {
   const [numPages, setNumPages] = useState(null);
@@ -40,20 +41,12 @@ const PDFPreview = () => {
           />
         </Document>
       </section>
-      <section className="flex justify-center items-center text-center">
-        <button
-          className="mx-5 text-gray-400 hover:scale-110 transform transition-transform hover:text-azulClaro-0 hover:font-bold"
-          onClick={handlePrev}
-          disabled={pageNumber === 1}
-        >
-          {"<"}
+      <section className="h-full w-full flex justify-center items-center text-center">
+        <button onClick={handlePrev} disabled={pageNumber === 1}>
+          <AiFillCaretLeft className="h-5 w-5 mx-5 flex justify-center items-center text-center text-gray-400 hover:scale-105 transform transition-transform hover:text-azulClaro-0 hover:font-bold" />
         </button>
-        <button
-          className="mx-5 text-gray-400 hover:scale-105 transform transition-transform hover:text-azulClaro-0 hover:font-bold"
-          onClick={handleNext}
-          disabled={pageNumber === numPages}
-        >
-          {">"}
+        <button onClick={handleNext} disabled={pageNumber === numPages}>
+          <AiFillCaretRight className="h-5 w-5 mx-5 flex justify-center items-center text-center text-gray-400 hover:scale-105 transform transition-transform hover:text-azulClaro-0 hover:font-bold" />
         </button>
       </section>
     </div>
